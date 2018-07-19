9dacc6631975fd216db3f214e1c1698ed768d084
Felix Dubrownik <felix@light-instruments.de>
2018-07-19 22:11:55 +0200

    print connector modes

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ extern crate drm;
 use std::fs::{OpenOptions, File};
 use std::os::unix::io::{AsRawFd, RawFd};
 
+use drm::control::ResourceInfo;
+use drm::control::ResourceHandle;
+use drm::control::{connector, crtc, dumbbuffer, framebuffer};
+
 use drm::Device as BasicDevice;
 use drm::control::Device as ControlDevice;
+
+
 
 // The drm crate does not provide a method of opening the device.
 // It is expected to be implemented by the user.
@@ -38,25 +44,60 @@ impl  DrmCard {
     }
 }
 
+fn load_information<T, U>(card: &Card, handles: &[T]) -> Vec<U>
+    where
+        T: ResourceHandle,
+        U: ResourceInfo<Handle = T>,
+{
+    handles
+        .iter()
+        .map(|&h| card.resource_info(h).expect("Could not load resource info"))
+        .collect()
+}
+
 fn main() {
     println!("Hello World");
     let card = DrmCard::open_global();
     // Get a set of all modesetting resource handles (excluding planes):
     let res_handles = card.resource_handles().unwrap();
 
+    // Load the information.
+    let res = card.resource_handles()
+        .expect("Could not load normal resource ids.");
+    let coninfo: Vec<connector::Info> = load_information(&card, res.connectors());
+    let crtcinfo: Vec<crtc::Info> = load_information(&card, res.crtcs());
+
+    for &info in coninfo.iter() {
+        for &mode in &info.modes().iter() {
+            println!("Connector Mode:");
+            println!(":?", mode);
+        }
+    }
+    
+    // Filter each connector until we find one that's connected.
+//    let con = coninfo
+//        .iter()
+//        .filter(|&i| i.connection_state() == connector::State::Connected)
+//        .next()
+//        .expect("No Connected Connectors");
+//
+//    for &info in con {
+//        println!(":?",info);
+//    }
+
     // Print all connector information
-    for &con in res_handles.connectors() {
-        let info :drm::control::connector::Info = card.resource_info(con).unwrap();
-
-        println!("{:#?}", info)
-    }
-
-    // Print all CRTC information
-    for &crtc in res_handles.crtcs() {
-        let info : drm::control::crtc::Info = card.resource_info(crtc).unwrap();
-
-        println!("{:#?}", info)
-    }
+//    for &con in res_handles.connectors() {
+//        let info : drm::control::connector::Info = card.resource_info(con).unwrap();
+//        for info.modes
+//        println!("{:#?}", info)
+//    }
+//
+//    // Print all CRTC information
+//    for &crtc in res_handles.crtcs() {
+//        let info : drm::control::crtc::Info = card.resource_info(crtc).unwrap();
+//
+//        println!("{:#?}", info)
+//    }
 
 
 }
